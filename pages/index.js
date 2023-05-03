6b476641a8ca1fb22ed33b2bebd45727853e384c
siddeshshewde <siddesh.shewde@gmail.com>
2023-05-03 07:58:17 -0400

    Added Rates for Remitly

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-const inter = Inter({ subsets: ['latin'] })
+const fetch = require('node-fetch');
 
-export default function Home() {
+import {useEffect, useState} from 'react';
+
+export default function Home(props) 
+{
+
   return (
     <>
       <Head>
@@ -14,101 +18,71 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+        {/* <div>Remitly Rate: {getReddit}</div> */}
+        {/* <div>Remitly Rate: {JSON.stringify(b.props)}</div> */}
+        <div>Remitly Rate: {props.titleList}</div>
+        <div>Last scraped: {props.lastScraped}</div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
     </>
   )
 }
+
+export async function getStaticProps ()
+{
+  const response = await fetch('https://www.remitly.com/us/en/india/pricing', {
+    cache: 'no-cache',
+    mode: 'no-cors',
+    headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    }});
+
+	const body = await response.text();
+
+  const $ = cheerio.load(body);
+  const titleList = [];
+
+  $('.fheif50').each((i, rate) => {
+    const titleNode = $(rate);
+    const titleText = titleNode.text();
+    
+    titleList.push(titleText);
+  });
+
+  console.log(titleList);
+  const lastScraped = new Date().toISOString()
+
+  console.log(lastScraped);
+	//return titleList;
+  return {
+    props: {titleList, lastScraped}
+  } 
+};
+
+
+// export async function remitly () 
+// {
+//   const AXIOS_OPTIONS = {
+//     headers: {
+//       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+//     }
+//   };
+
+//   axios.get('https://httpbin.org/headers', config) 
+// 	.then(({ data }) => { 
+// 		console.log(data) 
+// 	});
+// }
+
+//   //let data = await axios.get('https://www.remitly.com/us/en/india/pricing', AXIOS_OPTIONS);
+//   let data = await axios.get('https://www.npmjs.com/package/cors', AXIOS_OPTIONS);
+//   let $ = load(data);
+//   //let remitly_rate = $('/html/body/div[2]/div/div/div[2]/div[2]/h2[3]')
+//   let remitly_rate = $('/html/body/div[1]/div/div[2]/main/div/div[1]/h2');
+//   const lastScraped = new Date().toISOString()
+
+//   return {
+//     props: {remitly_rate, lastScraped}
+//   }
+// }
